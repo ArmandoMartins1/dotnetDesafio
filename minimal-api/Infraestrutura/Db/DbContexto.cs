a2542edbb897b191b28e7757d9c4648ddb1df2b6
Armando Alves <133614695+ArmandoMartins1@users.noreply.github.com>
2025-08-27 16:59:20 -0300

    fix: ajustes na DbContext para conexão no DB

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -9,14 +9,24 @@ namespace minimal_api.Infraestrutura.Db
 {
     public class DbContexto : DbContext
     {
+        public readonly IConfiguration _configuracaoAppSettings;
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
         public DbSet<Adminstrador> Adminstradores { get; set; } = default!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Esse "?" serve para que se ele não validar, ele volta vazio.
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
             optionsBuilder.UseMySql(
-                "string de conexão",
-            ServerVersion.AutoDetect("string de conexão")
+                stringConexao,
+            ServerVersion.AutoDetect(stringConexao)
             );
+            }
         }
     }
 }
