008daccaaaa366051b832157ea3d94db10e1ea20
Armando Alves <133614695+ArmandoMartins1@users.noreply.github.com>
2025-09-01 17:08:03 -0300

    validando login

---
+++ minimal-api/Dominio/Interfaces/IAdministradorServico.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;    
+namespace minimal_api.Dominio.Interfaces
+{
+    public interface IAdministradorServico
+    {
+        Adminstrador? Login(LoginDTO loginDTO);
+    }
+}
--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,10 +1,16 @@
 
 
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Dominio.Servicos;
 using minimal_api.Infraestrutura.Db;
+using minimal_api.Migrations;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options =>{
     options.UseMySql(
@@ -18,9 +24,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (minimal_api.Dominio.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorServico.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso!");
     else
         return Results.Unauthorized();
